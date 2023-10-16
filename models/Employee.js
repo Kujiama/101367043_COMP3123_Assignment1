d6ede59689879e2a359c6a0e98e08a63c9a0e0f5
Kujiama <englandpelenio17@gmail.com>
2023-10-15 20:35:29 -0400

    initial commit

---
+++ models/Employee.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+var EmployeeSchema = new mongoose.Schema({
+
+    firstName: {type:String,maxLength:100,required: true},
+    lastName:{type: String,maxLength:50,required: true},
+    email:{type: String,maxLength:50,required: true,unique:true},
+
+    //enum is used to restrict the values of the field
+    gender:{type: String,maxLength:25,lowercase:true,required: true,enum:["male","female","other"]},
+    
+    salary:{type: Number,required: true}
+    
+});
+
+module.exports = mongoose.model("Employee", EmployeeSchema);
